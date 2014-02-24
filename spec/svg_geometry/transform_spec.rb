2f364b398f2d0d5cb75e81c78ba7379f966a7ce4
pedrobello <pedro@ubee.in>
2014-02-24 17:10:54 -0300

    bugfix

--- spec/svg_geometry/transform_spec.rb
+++ spec/svg_geometry/transform_spec.rb
@@ -7,7 +7,7 @@ describe SvgGeometry::Transform do
   describe '#parse' do
     
     it "should parse properly" do
-      transform_str = "scale(10), translate(10,10) matrix(1,2,3,4,5,6) rotate(30) skewX(20) skewY(50) scale(3,4)"
+      transform_str = "scale(3,4) skewY(50) skewX(20) rotate(30) matrix(1,2,3,4,5,6) translate(10,10) scale(10)"
       transform = Transform.new(transform_str)
 
       expect(transform.transformations.length).to eq(7)
@@ -19,7 +19,7 @@ describe SvgGeometry::Transform do
                                              Transform::Matrix.new([1,tan(Util.degrees_to_rad(50)),0,1,0,0]),
                                              Transform::Scale.new(3,4)]))
 
-      transform_str = "scale(10), translate(10 10)  , matrix(1 2 3 4 5 6) rotate(30),scale(3,4),scale(3,4) skewX(20) skewY(50)"
+      transform_str = "skewY(50) skewX(20) scale(3,4),scale(3,4),rotate(30), matrix(1 2 3 4 5 6) translate(10 10) scale(10)"
       transform = Transform.new(transform_str)
 
       expect(transform.transformations.length).to eq(8)
